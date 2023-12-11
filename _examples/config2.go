da387dd9e3a600e53857b97b489e36bdcb26bd16
Kirill Che. <g4s8.public@gmail.com>
2023-12-11 14:21:14 +0400

    doc: improve examples

--- _examples/config2.go
+++ _examples/config2.go
@@ -5,13 +5,11 @@ type Config2 struct {
 	// Secret is a secret value that is read from a file.
 	Secret string `env:"SECRET,file"`
 	// Password is a password that is read from a file.
-	Password string `env:"PASSWORD,file"           envDefault:"/tmp/password"   json:"password"`
+	Password string `env:"PASSWORD,file"    envDefault:"/tmp/password"   json:"password"`
 	// Certificate is a certificate that is read from a file.
 	Certificate string `env:"CERTIFICATE,file,expand" envDefault:"${CERTIFICATE_FILE}"`
 	// Key is a secret key.
 	SecretKey string `env:"SECRET_KEY,required" json:"secret_key"`
 	// SecretVal is a secret value.
 	SecretVal string `json:"secret_val" env:"SECRET_VAL,notEmpty"`
-	// JustAMess is a mess of env options.
-	JustAMess string `fooo:"1" env:"JUST_A_MESS,required,notEmpty,file,expand" json:"just_a_mess" envDefault:"${JUST_A_MESS_FILE}" bar:"2"`
 }
--- _examples/config2.md
+++ _examples/config2.md
@@ -5,4 +5,3 @@
 - `CERTIFICATE` (expand, from file, default: `${CERTIFICATE_FILE}`) - Certificate is a certificate that is read from a file.
 - `SECRET_KEY` (**required**) - Key is a secret key.
 - `SECRET_VAL` (**required**, not-empty) - SecretVal is a secret value.
-- `JUST_A_MESS` (**required**, expand, not-empty, from file, default: `${JUST_A_MESS_FILE}`) - JustAMess is a mess of env options.
