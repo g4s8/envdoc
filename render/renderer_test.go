4020df5e73328ce2829112e7ea1f3b8a85511e51
Ruona Onobrakpeya <rhodeondev@gmail.com>
2025-11-12 18:50:52 +0100

    Added test for rendering of a custom template.

--- render/renderer_test.go
+++ render/renderer_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/g4s8/envdoc/types"
 )
 
-func TestRenderer(t *testing.T) {
+func TestRender(t *testing.T) {
 	r := NewRenderer(types.OutFormatTxt, false)
 	scopes := []*types.EnvScope{
 		{
@@ -40,6 +40,54 @@ func TestRenderer(t *testing.T) {
 	expectSb.WriteString("## scope1\n\n")
 	expectSb.WriteString("scope1 doc\n\n")
 	expectSb.WriteString(" * `VAR1` (required) - VAR1 doc\n")
+	expectSb.WriteString("\n")
+
+	if expect, actual := expectSb.String(), sb.String(); actual != expect {
+		t.Logf("Expected:\n%s", expect)
+		t.Logf("Got:\n%s", actual)
+		t.Fatalf("Unexpected output")
+	}
+}
+
+func TestRenderCustom(t *testing.T) {
+	r := NewRenderer(types.OutFormatTxt, false)
+	scopes := []*types.EnvScope{
+		{
+			Name: "scope1",
+			Doc:  "scope1 doc",
+			Vars: []*types.EnvDocItem{
+				{
+					Name: "VAR1",
+					Doc:  "VAR1 doc",
+					Opts: types.EnvVarOptions{
+						Required: true,
+					},
+				},
+			},
+		},
+	}
+
+	tmplPath := "testdata/customtext.tmpl"
+
+	var sb strings.Builder
+
+	if err := r.RenderCustom(scopes, tmplPath, &sb); err != nil {
+		t.Fatalf("Failed to render: %s", err)
+	}
+
+	// Environment Variables
+
+	// scope1
+
+	// scope1 doc
+
+	// - `VAR1` (required): VAR1 doc (REQUIRED)
+
+	var expectSb strings.Builder
+	expectSb.WriteString("Environment Variables\n\n")
+	expectSb.WriteString("## scope1\n\n")
+	expectSb.WriteString("scope1 doc\n\n")
+	expectSb.WriteString("- `VAR1` (REQUIRED) - VAR1 doc\n")
 	expectSb.WriteString("\n")
 
 	if expect, actual := expectSb.String(), sb.String(); actual != expect {
